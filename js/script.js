ddbbe9d28715a26ce24c6707d4d6559288e98f2a
Mustapha <56976902+MustaphaIdrissi@users.noreply.github.com>
2023-07-02 12:42:27 +0100

    Update script.js
    dd

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-const menuicon =document.querySelector(".mobile-menu-toggle");
-const navbar=document.querySelector(".menu-wrap");
-menuicon.addEventListener("click",()=>{
-    navbar.classList.toggle("show-hide");
-});
-
-
-
-
-
-  
 const darkmode =document.querySelector(".darkmode-toggle");
 const bod =document.querySelector("body");
 darkmode.addEventListener("click",()=>{
     bod.classList.toggle("darksum");
+});
+
+
+const menac=document.querySelector(".menu-wrap ul").querySelectorAll("a");
+
+menac.forEach(act=>{
+    act.addEventListener("click",()=>{
+        menac.forEach(nav=>nav.classList.remove("active"));
+        act.classList.add("active");
+    });
+   
+
 });
 
 
